b83de4680143a5e715240ce548741b4f7d9314c1
Esteban Muñoz Lagos <emunoz.ccp@gmail.com>
2016-05-03 21:58:07 -0300

    Lista de Items principal lista

--- app/src/main/java/cl/luckio/estacionesderadiosoffline/MainActivity.java
+++ app/src/main/java/cl/luckio/estacionesderadiosoffline/MainActivity.java
@@ -2,12 +2,26 @@ package cl.luckio.estacionesderadiosoffline;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lvRadios;
+    private String listaRadios[] = new String[]{"Radio Bio Bio", "ADN Radio"};
+    private Integer[] imgid={
+            R.drawable.biobio,
+            R.drawable.adn
+    };
+
+    private ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RadiosListAdapter adapter = new RadiosListAdapter(this, listaRadios, imgid);
+        lvRadios = (ListView) findViewById(R.id.lvRadios);
+        lvRadios.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/cl/luckio/estacionesderadiosoffline/RadiosListAdapter.java
@@ -0,0 +1,45 @@
+package cl.luckio.estacionesderadiosoffline;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Luckio on 03/05/2016.
+ */
+public class RadiosListAdapter extends ArrayAdapter<String>{
+
+    private final Activity context;
+    private final String[] itemname;
+    private final Integer[] integers;
+
+    public RadiosListAdapter(Activity context, String[] itemname, Integer[] integers) {
+        super(context, R.layout.lista_radio, itemname);
+        this.context = context;
+        this.itemname = itemname;
+        this.integers = integers;
+    }
+
+    public View getView(int posicion,View view, ViewGroup parent){
+
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.lista_radio, null, true);
+
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.texto_principal);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+        TextView etxDescripcion = (TextView) rowView.findViewById(R.id.texto_secundario);
+
+        txtTitle.setText(itemname[posicion]);
+        imageView.setImageResource(integers[posicion]);
+        etxDescripcion.setText("Description "+itemname[posicion]);
+
+        return rowView;
+    }
+}
