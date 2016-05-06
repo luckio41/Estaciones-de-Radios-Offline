3c98fcdf080847bb8ed1fe64ace42a7666715ef5
Esteban Muñoz Lagos <emunoz.ccp@gmail.com>
2016-05-06 19:31:55 -0300

    Result Activity ya se conecta al gps y muestra informe de Latitud y Longitud en pantalla

---
+++ app/src/main/java/cl/luckio/estacionesderadiosoffline/GpsService.java
@@ -0,0 +1,89 @@
+package cl.luckio.estacionesderadiosoffline;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by Luckio on 06/05/2016.
+ */
+public class GpsService extends Service implements LocationListener {
+
+    private final Context ctx;
+    double lat;
+    double len;
+    Location location;
+    boolean activeGps;
+    TextView tvCoordinates;
+    LocationManager locationManager;
+
+    public GpsService() {
+        super();
+        this.ctx = this.getApplicationContext();
+    }
+
+    public GpsService(Context c) {
+        super();
+        this.ctx = c;
+        getLocation();
+    }
+
+    public void setView(View view) {
+        tvCoordinates = (TextView) view;
+        tvCoordinates.setText("Lat: " + lat + " Len: " + len);
+    }
+
+    public void getLocation() {
+        try {
+            locationManager = (LocationManager) this.ctx.getSystemService(LOCATION_SERVICE);
+            activeGps = locationManager.isProviderEnabled(locationManager.GPS_PROVIDER);
+        } catch (Exception e) {
+        }
+
+        if (activeGps)
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 1000 * 60, 10, this);
+                location = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+                lat = location.getLatitude();
+                len = location.getLongitude();
+                return;
+            }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
--- app/src/main/java/cl/luckio/estacionesderadiosoffline/ResultsActivity.java
+++ app/src/main/java/cl/luckio/estacionesderadiosoffline/ResultsActivity.java
@@ -1,16 +1,26 @@
 package cl.luckio.estacionesderadiosoffline;
 
-import android.support.v4.app.NavUtils;
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
 
-    private int id;
     private TextView tvCoordinates;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +28,37 @@ public class ResultsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         tvCoordinates = (TextView) findViewById(R.id.tvCoordinates);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                tvCoordinates.setText("Latitud: " + location.getLatitude() + " Longitud: " + location.getLongitude());
+            }
 
-        Bundle extras = getIntent().getExtras();
-        id = extras.getInt("position");
-        tvCoordinates.setText( String.valueOf(id));
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET
+            }, 10);
+            return;
+        } else {
+            locationManager.requestLocationUpdates("gps", 1000, 0, locationListener);
+        }
     }
-
 }
