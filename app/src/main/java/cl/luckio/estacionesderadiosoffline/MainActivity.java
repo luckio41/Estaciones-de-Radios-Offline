4eb23a2d2f38064599e80e5b336217f8071b0d00
Esteban Muñoz Lagos <emunoz.ccp@gmail.com>
2016-05-12 15:57:49 -0300

    Ahora la llamada a gps se realiza en el main y se pasa por parametro al resultMain

--- app/src/main/java/cl/luckio/estacionesderadiosoffline/MainActivity.java
+++ app/src/main/java/cl/luckio/estacionesderadiosoffline/MainActivity.java
@@ -1,10 +1,15 @@
 package cl.luckio.estacionesderadiosoffline;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +40,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Location manager
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationManager.getAllProviders();
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                Toast.makeText(getApplicationContext(), String.valueOf(latitude), Toast.LENGTH_SHORT).show();
+                location.getProvider();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET
+            }, 10);
+            return;
+        }
+        else {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
+        }
 
         SqlHelper sqlHelper = new SqlHelper(this, "ESTACIONESDB", null, 1);
         SQLiteDatabase db = sqlHelper.getReadableDatabase();
@@ -83,16 +130,6 @@ public class MainActivity extends AppCompatActivity {
     private void ShowResults(int position, View view) {
         Intent i = new Intent(this, ResultsActivity.class);
 
-        if(latitude > 0)
-            latitude = latitude;
-        else
-            latitude = 0;
-
-        if(longitude > 0)
-            longitude = longitude;
-        else
-            longitude = 0;
-
         i.putExtra("position", position);
         i.putExtra("latitude", latitude);
         i.putExtra("longitude", longitude);
@@ -102,5 +139,29 @@ public class MainActivity extends AppCompatActivity {
     public void AcercaDe(){
         Intent intent = new Intent(this, AcercaDe.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.removeUpdates(locationListener);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET
+            }, 10);
+            return;
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
+        }
     }
 }
--- app/src/main/java/cl/luckio/estacionesderadiosoffline/ResultsActivity.java
+++ app/src/main/java/cl/luckio/estacionesderadiosoffline/ResultsActivity.java
@@ -2,34 +2,28 @@ package cl.luckio.estacionesderadiosoffline;
 
 import android.Manifest;
 import android.annotation.TargetApi;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
-import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ResultsActivity extends AppCompatActivity {
 
     private TextView tvCity;
     private TextView tvFrequency;
-    private LocationManager locationManager;
-    private LocationListener locationListener;
     private SqlHelper sqlHelper;
     private SQLiteDatabase db;
     private String nameCity;
     private String frequency;
-    private double latitude;
-    private double longitude;
+    private double _latitude;
+    private double _longitude;
     private int position;
     private ProgressBar progressBar;
     private TextView tvInfo;
@@ -42,8 +36,11 @@ public class ResultsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Bundle extras = getIntent().getExtras();
-        latitude = extras.getDouble("latitude");
-        longitude = extras.getDouble("longitude");
+        _latitude = extras.getDouble("latitude");
+        _longitude = extras.getDouble("longitude");
+
+        Toast.makeText(getApplicationContext(), "Re: " + String.valueOf(_latitude), Toast.LENGTH_SHORT).show();
+
         position = extras.getInt("position");
 
         tvCity = (TextView) findViewById(R.id.tvCity);
@@ -52,99 +49,33 @@ public class ResultsActivity extends AppCompatActivity {
         progressBar.setVisibility(View.VISIBLE);
         tvInfo = (TextView) findViewById(R.id.tvInfo);
 
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         sqlHelper = new SqlHelper(this, "ESTACIONESDB", null, 1);
         db = sqlHelper.getReadableDatabase();
 
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
+        if(db != null) {
+            String sql = "SELECT c.Name_city, c.Frequency FROM Cities c " +
+                    "INNER JOIN Stations s " +
+                    "ON (c.ID_Cities = s.ID) " +
+                    "WHERE s.ID = " + (position + 1) + " " +
+                    "AND " + _latitude + " <= c.North " +
+                    "AND " + _latitude + " >= c.South " +
+                    "AND " + _longitude + " >= c.West " +
+                    "AND " + _longitude + " <= c.East";
 
-                if(latitude > 0)
-                    latitude = latitude;
-                else
-                    latitude = location.getLatitude();
+            Cursor c = db.rawQuery(sql, null);
 
-                if(longitude > 0)
-                    longitude = longitude;
-                else
-                    longitude = location.getLongitude();
-
-                if(db != null){
-                    String sql = "SELECT c.Name_city, c.Frequency FROM Cities c " +
-                            "INNER JOIN Stations s " +
-                            "ON (c.ID_Cities = s.ID) " +
-                            "WHERE s.ID = "+(position+1)+" " +
-                            "AND "+ latitude +" <= c.North " +
-                            "AND "+ latitude +" >= c.South " +
-                            "AND "+ longitude +" >= c.West " +
-                            "AND "+ longitude +" <= c.East";
-
-                    Cursor c = db.rawQuery(sql, null);
-
-                    if (c.moveToFirst()){
-                        for (int i = 0; i < c.getCount() ; i ++) {
-                            nameCity = c.getString(0).toString();
-                            frequency = c.getString(1).toString();
-                            c.moveToNext();
-                        }
-                    }
-
-                    tvCity.setText(nameCity);
-                    tvFrequency.setText(frequency);
-                    progressBar.setVisibility(View.GONE);
-                    tvInfo.setText("");
-
+            if (c.moveToFirst()) {
+                for (int i = 0; i < c.getCount(); i++) {
+                    nameCity = c.getString(0).toString();
+                    frequency = c.getString(1).toString();
+                    c.moveToNext();
                 }
             }
 
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        };
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET
-            }, 10);
-            return;
-        } else {
-            locationManager.requestLocationUpdates("gps", 1000, 0, locationListener);
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        locationManager.removeUpdates(locationListener);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET
-            }, 10);
-            return;
-        } else {
-            locationManager.requestLocationUpdates("gps", 1000, 0, locationListener);
+            tvCity.setText(nameCity);
+            tvFrequency.setText(frequency);
+            progressBar.setVisibility(View.GONE);
+            tvInfo.setText("");
         }
     }
 }
