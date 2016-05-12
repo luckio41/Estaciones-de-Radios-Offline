72f6a235f16de06082c3634d9412d6edce5478e9
Esteban Muñoz Lagos <emunoz.ccp@gmail.com>
2016-05-11 23:48:13 -0300

    Hay que hacer que el gps carge al iniciar la app y pase los datos mediate intent a la pantalla de resultdos

--- app/src/main/java/cl/luckio/estacionesderadiosoffline/MainActivity.java
+++ app/src/main/java/cl/luckio/estacionesderadiosoffline/MainActivity.java
@@ -1,14 +1,10 @@
 package cl.luckio.estacionesderadiosoffline;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
